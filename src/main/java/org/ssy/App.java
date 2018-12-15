27534f07bbe442ab48e83de9694744988173c068
uMklami <m.aamir.it@gmail.com>
2018-12-16 01:32:47 +0800

    updated

--- src/main/java/org/ssy/App.java
+++ src/main/java/org/ssy/App.java
@@ -2,6 +2,7 @@ package org.ssy;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -12,37 +13,48 @@ import java.util.Set;
  *
  */
 public class App {
-	
+
 	public static List<String> packaging;
-	public static List<String> production ;
+	public static List<String> production;
 //	public static Map<Long, Integer> mainMap;
-	public static Long arr[] = {12345l, 12346l, 12347l, 12348l, 12349l};
+	public static Long arr[] = { 12345l, 12346l, 12347l, 12348l, 12349l };
 
 	public App() {
-		
+
 //		Collections.addAll(production, arr);
 	}
-	
 
 	public static void main(String[] args) {
 //		production = new HashSet<>(Arrays.asList(arr));
 //		packaging = new long[]{12354,12346,12345,12345,12346,12346,12349,12349,12349};
 //		System.out.println("Orignal data");
-		
-		 production = DataLoader.loadXmlData("production.xml");
-		 packaging = DataLoader.loadXmlData("packaging.xml");
-		
+
+		long stime = System.currentTimeMillis();
+
+		production = DataLoader.loadXmlData("production.xml");
+		packaging = DataLoader.loadXmlData("packaging.xml");
+
+		System.out.println("Total number of events in Production : " + production.size());
+		System.out.println("Total number of events in Packaging : " + packaging.size());
+		System.out.println("\n\n");
+
 		process(production, packaging);
-		
-		
+
+		long etime = System.currentTimeMillis();
+
+		long overalltime = etime - stime;
+
+//		DecimalFormat df = new DecimalFormat("#.####");
+
+		System.out.print("Excuation time is : " + overalltime + " ms");
 
 	}
 
 	public static void process(List<String> p_production, List<String> p_packaging) {
-		
+
 		Map<String, Integer> mainMap = new HashMap<String, Integer>();
-//		Set<Long> outset = new HashSet<>();
-		
+		Set<String> itemloss = new HashSet<>();
+
 		for (String p_key : p_production) {
 			int ntimes = 0;
 			for (String p_value : new ArrayList<>(p_packaging)) {
@@ -54,41 +66,50 @@ public class App {
 //				System.out.println(p_key+" matched "+ntimes);
 			}
 //			System.out.println(p_key+" :: "+ntimes);
-			mainMap.put(p_key, ntimes);
+			if (ntimes == 0) {
+				itemloss.add(p_key);
+			} else {
+				mainMap.put(p_key, ntimes);
+			}
 		}
-		print(p_packaging,mainMap);
-	}
-	
-	public static void print(List<String> p_packaing, Map<String, Integer> mainMap2)
-	{
-		
-		System.out.println("Counter Feeds in Packaging");
-		for(String val : p_packaing)
-		{
-			System.out.print(val+", ");
+		System.out.println("Number of Item Loss in Production : " + itemloss.size());
+
+		System.out.println("ItemLoss are : ");
+		for (String val : itemloss) {
+			System.out.print(val + ", ");
 		}
 		System.out.println("\n\n");
+
 		
+		print(p_packaging, mainMap);
+	}
+
+	public static void print(List<String> p_packaging, Map<String, Integer> mainMap2) {
+		System.out.println("Number of Counter Feeds in Packaging : " + p_packaging.size());
+
+		System.out.println("Counter Feeds in Packaging");
+		for (String val : p_packaging) {
+			System.out.print(val + ", ");
+		}
+		System.out.println("\n\n");
+
 		System.out.println("Matched Items");
-		for(Entry<String,Integer> pair : mainMap2.entrySet())
-		{
-			System.out.println(pair.getKey()+" : "+pair.getValue());
+
+		for (Entry<String, Integer> pair : mainMap2.entrySet()) {
+			System.out.println(pair.getKey() + " : " + pair.getValue());
 		}
 	}
-	
-	public static void print(Set<Long> p_production, long[] p_packaing)
-	{
+
+	public static void print(Set<Long> p_production, long[] p_packaing) {
 		System.out.println("Prodcution");
-		for(long key : p_production)
-		{
-			System.out.print(key+", ");
+		for (long key : p_production) {
+			System.out.print(key + ", ");
 		}
 		System.out.println("\n");
-		
+
 		System.out.println("Packaging");
-		for(long val : p_packaing)
-		{
-			System.out.print(val+", ");
+		for (long val : p_packaing) {
+			System.out.print(val + ", ");
 		}
 		System.out.println("\n\n");
 	}
